a815d59a73b4a50cd4d7ab4f61e4bb97f7aa2c46
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-04-07 20:49:27 +0530

    create map_page.dart and update it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_map/pages/map_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MapPage()
     );
   }
 }
