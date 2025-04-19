42470fdf63efb19d66bc666ad5121d0bbd8498f9
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-04-19 22:11:00 +0530

    Update map_page.dart
    Add polylines, destination time

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -5,7 +5,8 @@ import 'package:location/location.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_place/google_place.dart';
 import 'package:location/location.dart' as loc;
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const String GOOGLE_MAPS_API_KEY = "AIzaSyDPPGBYGwYTOrWtL9dNmiXkjhrsGS6sFTY"; // Don't forget to replace
 
@@ -21,21 +22,24 @@ class _MapPageState extends State<MapPage> {
   final Completer<GoogleMapController> _mapController = Completer<GoogleMapController>();
   final TextEditingController _searchController = TextEditingController();
 
-  //static const LatLng _pGooglePlex = LatLng(6.927079, 79.861244);
-  //static const LatLng _pApplePark = LatLng(6.933850, 79.844860);
+  static const LatLng _pGooglePlex = LatLng(6.927079, 79.861244);
+  static const LatLng _pApplePark = LatLng(6.933850, 79.844860);
   LatLng? _currentP;
+  LatLng? _destinationP;
 
   Map<PolylineId, Polyline> polylines = {};
+  Map<MarkerId, Marker> markers = {};
   late GooglePlace googlePlace;
   List<AutocompletePrediction> predictions = [];
+
+  String? travelTime;
+  String? travelDistance;
 
   @override
   void initState() {
     super.initState();
     googlePlace = GooglePlace('AIzaSyBbLd67HZT9EYb8Xi7ySuVWJVBmLRFREjM');
-    /*getLocationUpdates().then((_) => {
-      getPolylinePoints().then((coodinates) => generatePolylineFromPoints(coodinates)),
-    });*/
+    getLocationUpdates();
   }
 
   @override
@@ -48,11 +52,7 @@ class _MapPageState extends State<MapPage> {
           GoogleMap(
             onMapCreated: (controller) => _mapController.complete(controller),
             initialCameraPosition: CameraPosition(target: _currentP!, zoom: 11),
-            markers: {
-              Marker(markerId: const MarkerId("_currentLocation"), position: _currentP!),
-             // Marker(markerId: const MarkerId("_sourceLocation"), position: _pGooglePlex),
-             // Marker(markerId: const MarkerId("_destinationLocation"), position: _pApplePark),
-            },
+            markers: Set<Marker>.of(markers.values),
             polylines: Set<Polyline>.of(polylines.values),
           ),
 
@@ -109,12 +109,17 @@ class _MapPageState extends State<MapPage> {
                               LatLng newPos = LatLng(location.lat!, location.lng!);
 
                               setState(() {
-                                _currentP = newPos;
+                                _destinationP = newPos;
                                 predictions = [];
                                 _searchController.clear();
                               });
 
                               _cameraToPosition(newPos);
+
+                              // Draw route to the selected destination
+                              if (_currentP != null) {
+                                updateDirections(_currentP!, _destinationP!);
+                              }
                             }
                           },
                         );
@@ -124,6 +129,60 @@ class _MapPageState extends State<MapPage> {
               ],
             ),
           ),
+
+          // Travel Time Info Panel
+          if (travelTime != null && travelDistance != null)
+            Positioned(
+              bottom: 20,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.access_time, color: Colors.blue),
+                        const SizedBox(width: 8),
+                        Text(
+                          travelTime!,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.directions_car, color: Colors.blue),
+                        const SizedBox(width: 8),
+                        Text(
+                          travelDistance!,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
         ],
       ),
     );
@@ -152,20 +211,103 @@ class _MapPageState extends State<MapPage> {
       if (currentLocation.latitude != null && currentLocation.longitude != null) {
         setState(() {
           _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
-          _cameraToPosition(_currentP!);
+
+          // Update current location marker
+          final MarkerId markerId = MarkerId("_currentLocation");
+          final Marker marker = Marker(
+            markerId: markerId,
+            position: _currentP!,
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(title: "Current Location"),
+          );
+          markers[markerId] = marker;
+
+          // Add fixed markers
+          markers[MarkerId("_sourceLocation")] = Marker(
+            markerId: MarkerId("_sourceLocation"),
+            position: _pGooglePlex,
+          );
+
+          markers[MarkerId("_destinationLocation")] = Marker(
+            markerId: MarkerId("_destinationLocation"),
+            position: _pApplePark,
+          );
+
+          // If destination is already set, update the polyline
+          if (_destinationP != null) {
+            updateDirections(_currentP!, _destinationP!);
+          }
         });
       }
     });
   }
 
- /* Future<List<LatLng>> getPolylinePoints() async {
+  Future<void> updateDirections(LatLng start, LatLng end) async {
+    // Get travel time and route information
+    await getDirectionDetails(start, end);
+
+    // Get polyline coordinates
+    List<LatLng> polylineCoordinates = await getPolylinePoints(start, end);
+
+    // Update polyline on map
+    generatePolylineFromPoints(polylineCoordinates);
+
+    // Add destination marker
+    final MarkerId markerId = MarkerId("_searchDestination");
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: end,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+      infoWindow: InfoWindow(
+        title: "Destination",
+        snippet: travelTime,
+      ),
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  Future<void> getDirectionDetails(LatLng start, LatLng end) async {
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?'
+        'origin=${start.latitude},${start.longitude}'
+        '&destination=${end.latitude},${end.longitude}'
+        '&mode=driving'
+        '&key=$GOOGLE_MAPS_API_KEY';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        if (data['status'] == 'OK') {
+          setState(() {
+            // Extract travel time and distance from the response
+            travelTime = data['routes'][0]['legs'][0]['duration']['text'];
+            travelDistance = data['routes'][0]['legs'][0]['distance']['text'];
+          });
+        } else {
+          debugPrint('Direction API error: ${data['status']}');
+        }
+      } else {
+        debugPrint('Failed to fetch directions: ${response.statusCode}');
+      }
+    } catch (e) {
+      debugPrint('Error getting directions: $e');
+    }
+  }
+
+  Future<List<LatLng>> getPolylinePoints(LatLng start, LatLng end) async {
     List<LatLng> polylineCoordinates = [];
     PolylinePoints polylinePoints = PolylinePoints();
 
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       GOOGLE_MAPS_API_KEY,
-      PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),
-      PointLatLng(_pApplePark.latitude, _pApplePark.longitude),
+      PointLatLng(start.latitude, start.longitude),
+      PointLatLng(end.latitude, end.longitude),
       travelMode: TravelMode.driving,
     );
 
@@ -181,17 +323,21 @@ class _MapPageState extends State<MapPage> {
   }
 
   void generatePolylineFromPoints(List<LatLng> polylineCoordinates) {
-    PolylineId id = const PolylineId("poly");
+    // Clear previous polylines
+    polylines.clear();
+
+    PolylineId id = const PolylineId("route");
     Polyline polyline = Polyline(
       polylineId: id,
-      color: Colors.black,
+      color: Colors.blue,
       points: polylineCoordinates,
-      width: 8,
+      width: 5,
     );
+
     setState(() {
       polylines[id] = polyline;
     });
-  }*/
+  }
 
   void autoCompleteSearch(String value) async {
     var result = await googlePlace.autocomplete.get(value);
