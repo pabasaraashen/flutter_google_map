95862d51b4bcf58f738a0aadfca922598f19974f
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-04-19 09:12:05 +0530

    update map_page.dart, pubspec.yaml files
    change the pipeline version

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,142 @@
 import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+const String GOOGLE_MAPS_API_KEY = "AIzaSyDPPGBYGwYTOrWtL9dNmiXkjhrsGS6sFTY";
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  Location _locationController = new Location();
+
+  final Completer<GoogleMapController> _mapController =
+  Completer<GoogleMapController>();
+
+  static const LatLng _pGooglePlex = LatLng(6.927079, 79.861244);
+  static const LatLng _pApplePark = LatLng(6.933850, 79.844860);
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates().then((_) => {
+      getPolylinePoints().then((coodinates) =>
+          print(coodinates)
+      ),
+    },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _currentP == null
+          ? const Center(
+        child: Text("Loading..."),
+      )
+          : GoogleMap(
+        onMapCreated: ((GoogleMapController controller) =>
+            _mapController.complete(controller)),
+        initialCameraPosition:  CameraPosition(
+          target: _currentP!,
+          zoom: 11,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _currentP!,
+          ),
+          Marker(
+            markerId: const MarkerId("_sourceLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pGooglePlex,
+          ),
+          Marker(
+            markerId: const MarkerId("_destinationLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pApplePark,
+          ),
+        },
+      ),
+    );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async{
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition = CameraPosition(
+      target: pos,
+      zoom: 11,
+    );
+    await controller.animateCamera(
+      CameraUpdate.newCameraPosition(_newCameraPosition),
+    );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    }else{
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await _locationController.requestPermission();
+      if(_permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged.listen((LocationData currentLocation) {
+      if(currentLocation. latitude != null &&
+          currentLocation.longitude != null){
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _cameraToPosition(_currentP!);
+        });
+      }
+    } );
+  }
+
+
+  Future<List<LatLng>> getPolylinePoints() async{
+    List<LatLng> polylineCoordinates = [] ;
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      GOOGLE_MAPS_API_KEY,
+      PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),
+      PointLatLng(_pApplePark.latitude, _pApplePark.longitude),
+      travelMode: TravelMode.driving,
+    );
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    } else {
+      debugPrint("Error retrieving polyline: ${result.errorMessage}");
+    }
+
+    return polylineCoordinates;
+  }
+}
+
+/*import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -113,7 +251,7 @@ class _MapPageState extends State<MapPage> {
     final GoogleMapController controller = await _mapController.future;
     CameraPosition newCameraPosition = CameraPosition(
       target: pos,
-      zoom: 14,
+      zoom: 11,
     );
     await controller.animateCamera(
       CameraUpdate.newCameraPosition(newCameraPosition),
@@ -156,27 +294,24 @@ class _MapPageState extends State<MapPage> {
   // Get polyline points between source and destination
   Future<List<LatLng>> getPolylinePoints() async {
     List<LatLng> polylineCoordinates = [];
+
     PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      GOOGLE_MAPS_API_KEY,  // First positional parameter (API key)
+      PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),  // Second positional parameter (origin)
+      PointLatLng(_pApplePark.latitude, _pApplePark.longitude),  // Third positional parameter (destination)
+      travelMode:TravelMode.driving,  // Named parameter
+    );
 
-    try {
-      PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        GOOGLE_MAPS_API_KEY,
-        PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),
-        PointLatLng(_pApplePark.latitude, _pApplePark.longitude),
-        travelMode: TravelMode.driving,
-      );
-
-      if (result.points.isNotEmpty) {
-        for (var point in result.points) {
-          polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-        }
-      } else {
-        debugPrint("Error retrieving polyline: ${result.errorMessage}");
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
       }
-    } catch (e) {
-      debugPrint("Exception while getting polyline points: $e");
+    } else {
+      debugPrint("Polyline error: ${result.errorMessage}");
     }
 
     return polylineCoordinates;
   }
-}
+
+}*/
